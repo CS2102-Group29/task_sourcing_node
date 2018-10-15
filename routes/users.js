bfdaf83f0e2cd5bc2e886fd3cff81deea71dc3f7
Calvin Tantio <calvin.tantio@outlook.com>
2018-10-15 18:00:17 +0800

    Changed back

--- routes/users.js
+++ routes/users.js
@@ -67,8 +67,8 @@ router.post('/updateinfo/:email', (req, res) => {
     dbClient.query(`UPDATE users 
                     SET email = '${email}', name = '${name}', mobile = '${mobile}'
                     WHERE email = '${old_email}'`, (err, dbres) => {
-                        if(err) {
-                            res.json({ success: false, msg: err.code })
+                        if(err && err.code === UNIQUE_VIOLATION) {
+                            res.json({ success: false, msg: 'User with the specified email already exists.' })
                         } else {
                             res.json({ success: true, msg: 'Personal particulars are updated successfully.' });
                         }
